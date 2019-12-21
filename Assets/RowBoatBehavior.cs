7455909bcefb417bdba7415a4a7b3963c53f581d
amelialutz9 <amelialutz9@gmail.com>
2019-12-21 17:13:47 -0600

    Add files via upload

---
+++ Assets/RowBoatBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RowBoatBehavior : MonoBehaviour
+{
+    public GameObject board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        print("collider: " + collider.tag);
+        SceneManager.LoadScene("poly_island");
+    }
+}
---
+++ Assets/exitWithButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exitWithButton : MonoBehaviour {
+
+    // Use this for initialization
+    private SteamVR_Controller.Device quitButton;
+    public GameObject pauseText;
+
+    public bool paused = false;
+    private float timeScale;
+    void Start () {
+        pauseText.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = timeScale;
+
+            }
+            Application.LoadLevel("Menu");
+            
+        }
+
+        if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost)).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = timeScale;
+
+            }
+            Application.LoadLevel("Menu");
+        }
+
+
+        if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost)).GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            if (!paused)
+            {
+                timeScale = Time.timeScale;
+                Time.timeScale = 0.0f;
+                paused = true;
+                pauseText.SetActive(true);
+            }
+            else
+            {
+                paused = false;
+                Time.timeScale = timeScale;
+                pauseText.SetActive(false);
+            }
+        }
+
+    }
+}
